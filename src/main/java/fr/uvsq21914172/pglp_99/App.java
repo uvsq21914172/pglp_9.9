b6954715347a97c9796ead854171bc6a5130436e
Dalil <dalil.tadjeddine@ens.uvsq.fr>
2020-05-15 05:59:03 +0200

    Create App.java

---
+++ src/main/java/fr/uvsq21914172/pglp_99/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq21914172.pglp_99;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
