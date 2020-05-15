e7a889913a7b5c21a07ff57a62fed5bbce05e906
Dalil <dalil.tadjeddine@ens.uvsq.fr>
2020-05-15 05:59:37 +0200

    Create Square.java

---
+++ src/main/java/fr/uvsq21914172/pglp_99/shape/Square.java
@@ -0,0 +1,24 @@
+package fr.uvsq21914172.pglp_99.shape;
+
+public class Square  extends AShape{
+  
+  int rayon;
+  
+  public Circle(int x, int y, int rayon) {
+    super(x,y);
+    this.rayon = rayon;
+  }
+  
+  public void setRayon(int rayon) {
+    this.rayon = rayon;
+  }
+  
+  public int getRayon() {
+    return rayon;
+  }
+  
+  public String display() {
+    return "Circle(centre="+position.display()+",rayon="+rayon+")";
+  }
+  
+}
