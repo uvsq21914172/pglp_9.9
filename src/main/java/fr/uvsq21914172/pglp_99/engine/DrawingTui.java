47fe5e664b4969d6bb0dff3625a15de60e9d90f3
Dalil <dalil.tadjeddine@ens.uvsq.fr>
2020-05-16 07:45:44 +0200

    Create DrawingTui.java

---
+++ src/main/java/fr/uvsq21914172/pglp_99/engine/DrawingTui.java
@@ -0,0 +1,51 @@
+package fr.uvsq21914172.pglp_99.engine;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+import fr.uvsq21914172.pglp_99.commands.Command;
+import fr.uvsq21914172.pglp_99.commands.CommandInput;
+
+public class DrawingTui {
+
+  private Drawing drawing = new Drawing();
+  private boolean state = true;
+  InputStream in = System.in;
+  OutputStream out = System.out;
+  Scanner sc = new Scanner(in);
+  
+  public InputStream getIn() {
+    return in;
+  }
+
+  public void setIn(InputStream in) {
+    this.in = in;
+  }
+
+  public void setDrawing(Drawing drawing) {
+    this.drawing = drawing;
+  }
+
+  public Command input() {
+    return CommandInput.commandParser(sc.nextLine(), this);
+  }
+  
+  public Drawing getDrawing() {
+    return drawing;
+  }
+
+  public void quit() {
+    state = false;
+  }
+  
+  public boolean getState() {
+    return state;
+  }
+
+  public boolean ask(String string) {
+    ((PrintStream) out).println(string);
+    return sc.nextLine().equalsIgnoreCase("y");
+  }
+
+}
